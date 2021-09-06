1cff23cdb97b482d86bea72df8a696824daf5d38
dim2019 <dmitriy.mezvrishvili@mail.ru>
2021-09-06 18:02:42 +0400

    optimized

--- day-16/HomeWork-2.js
+++ day-16/HomeWork-2.js
@@ -3,15 +3,22 @@ let i = 0;
 let numbers = [];
 var num = 0;
 // While ციკლით
-while (true) {
-    if (n / 2 < 50) {
-        break
-    } else {
-        n = n / 2
-        numbers.push(n)
-    }
-    i++
+// while (true) {
+//     if (n / 2 < 50) {
+//         break
+//     } else {
+//         n = n / 2
+//         numbers.push(n)
+//     }
+//     i++
+// }
+//ოპტიმიზირებული//
+while (n / 2 > 50) {
+    n = n / 2
+    num++
+
 }
+console.log(num);
 // FOR ციკლით
 // for (let i = 0; i < 1000; i++) {
 //     if (n / 2 < 50) {
@@ -21,5 +28,5 @@ while (true) {
 //         numbers.push(n)
 //     }
 // }
-num += numbers.length
-console.log(num);
+// num += numbers.length
+// console.log(num);
