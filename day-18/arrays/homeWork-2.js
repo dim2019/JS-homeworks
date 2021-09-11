c4f7fa8f415a50c503c228b563eea8c04e1efbe7
dim2019 <dmitriy.mezvrishvili@mail.ru>
2021-09-11 18:46:20 +0400

    day-18/arrays/homework-2

--- day-18/arrays/homeWork-2.js
+++ day-18/arrays/homeWork-2.js
@@ -1,9 +1,9 @@
-const arr = []
+const arr = {}
 let reverse = (array) => {
     let removed
     try {
-        if (typeof arr !== "object") {
-            throw new TypeError('parameter required and has to be only array')
+        if (!Array.isArray(array)) {
+            throw new TypeError('First parameter required and has to be only array')
         } else if (arr == '') {
             throw new TypeError('passed empty array')
         } else {
@@ -12,9 +12,9 @@ let reverse = (array) => {
                 array.unshift(removed);
             }
         }
-        return array.flat();
+        console.log(array.flat())
     } catch (err) {
         console.log(err.message);
     }
 }
-console.log(reverse(arr));
+reverse(arr)
