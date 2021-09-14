8a48b393eee965d980e2c0706c73d52f76bc2536
dim2019 <dmitriy.mezvrishvili@mail.ru>
2021-09-14 17:35:43 +0400

    day-19/Strings/Task-2

---
+++ day-19/Strings/homework-2.js
@@ -0,0 +1,7 @@
+function extractCurrencyValue(source) {
+    if (typeof source !== 'string') throw new TypeError("first parameter isn't string")
+    let str = source.substr(1)
+    return parseInt(str)
+}
+const value = extractCurrencyValue('$120')
+console.log(value);
