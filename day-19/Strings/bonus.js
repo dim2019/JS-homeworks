aedd710f6c29fa2747df927d9a363d3dc71aa945
dim2019 <dmitriy.mezvrishvili@mail.ru>
2021-09-14 18:26:19 +0400

    day-19/Strings/bonus

---
+++ day-19/Strings/bonus.js
@@ -0,0 +1,10 @@
+function searchWord(text, word) {
+    if (typeof text !== 'string' || typeof word !== 'string') throw new TypeError('params are not strings')
+    else if (text.includes(word))
+        var count = (text, word) => {
+            return text.split(word).length - 1;
+        }
+    return ('"' + "'" + word + "'" + ' was found ' + count(text, word) + ' times.' + '"')
+}
+const HTML = searchWord('The quick brown fox', 'fox')
+console.log(HTML);
