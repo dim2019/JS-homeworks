f609f7508f1db291da22dca31f7acf65f6b73966
dim2019 <dmitriy.mezvrishvili@mail.ru>
2021-09-15 13:52:57 +0400

    day-19/Objects/tast1

---
+++ day-19/Objects/homework-1.js
@@ -0,0 +1,32 @@
+function readingStatus(books) {
+    for (i in books) {
+        if (books[i].haveRead) console.log("'" + books[i].author + ' have read ' + books[i].title + ' book' + "'");
+        else console.log("'" + books[i].author + " haven't read " + books[i].title + ' book yet' + "'");
+
+    }
+
+}
+var Books = [{
+        author: 'Bill',
+        title: 'The Road Ahead',
+        haveRead: true,
+        dateOfRead: new Date(2020, 10, 10)
+    },
+    {
+        author: 'Steve',
+        title: 'Walter Isaacson',
+        haveRead: true,
+        dateOfRead: new Date(2020, 10, 5)
+    },
+    {
+        author: 'Jhon',
+        title: 'The Hunger Games',
+        haveRead: false,
+        dateOfRead: NaN
+    }
+]
+readingStatus(Books);
+
+// 'Bill have read The Road Ahead book'
+// 'Steve have read Walter Isaacson book'
+// 'Jhon haven't read The Hunger Games book yet'
