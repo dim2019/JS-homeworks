7e808c969b9b2e80a473190c68af682c95b258fd
dim2019 <dmitriy.mezvrishvili@mail.ru>
2021-09-29 18:57:51 +0400

    day-21/task-1/Fixed

--- day-21/homework-1.js
+++ day-21/homework-1.js
@@ -9,13 +9,13 @@ class Validator {
         else return true
     }
     isDomain(domain) {
-        var regexDomain = new RegExp(/^((?:(?:(?:\w[\.\-\+]?)*)\w)+)((?:(?:(?:\w[\.\-\+]?){0,62})\w)+)\.(\w{2,6})$/);
+        var regexDomain = /^(http:\/\/www\.|https:\/\/www\.|http:\/\/|https:\/\/)?[a-z0-9]+([\-\.]{1}[a-z0-9]+)*\.[a-z]{2,5}(:[0-9]{1,5})?(\/.*)?$/
         if (domain == '') return false
         else if (!(regexDomain.test(domain))) return false
         else return true
     }
     isDate(date) {
-        let regexDate = /^(0[1-9]|1[0-2])\/(0[1-9]|1\d|2\d|3[01])\/(19|20)\d{2}$/;
+        let regexDate = /^(0?[1-9]|[12][0-9]|3[01])[\/\-\.](0?[1-9]|1[012])[\/\-\.]\d{4}$/;
         if (date == '') return false
         else if (!(regexDate.test(date))) return false
         else return true
@@ -25,12 +25,13 @@ class Validator {
         let regexPhone = /^[+]*[(]{0,1}[995]{1,3}[)]{0,1}[-\s\./0-9]*$/g
         if (PhoneNumber == '') return false
         else if (!(regexPhone.test(PhoneNumber))) return false
+
         else return true
     }
 }
 var validator = new Validator();
 
 console.log(validator.isEmail('jshtml@mail.ru'));
-console.log(validator.isDomain('jshtml.net'));
+console.log(validator.isDomain('https://www.facebook.com'));
 console.log(validator.isDate('12.05.2020'));
-console.log(validator.isPhone('+(995)598-183-704'))
+console.log(validator.isPhone('+(995)598 18-37-04'))
