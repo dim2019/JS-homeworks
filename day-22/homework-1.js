f524691e965d60396ff17f06b9fffef4d84d999c
dim2019 <dmitriy.mezvrishvili@mail.ru>
2021-10-03 21:19:02 +0400

    done...

---
+++ day-22/homework-1.js
@@ -0,0 +1,22 @@
+const get = require('fetch').fetchUrl
+const send = url =>
+    new Promise((resolve, reject) => {
+        get(url, (error, meta, body) => {
+            if (meta.status == 200) {
+                const { data } = JSON.parse(body)
+                resolve(`This parameter is an array that contains a list of country objects`)
+            }
+            reject(`We have error, status code: ${meta.status}`)
+        })
+    });
+
+
+const url = 'https://api.openweathermap.org/data/2.5/weather?q=London&appid=299fb2133133f9d8fc214f5ae28ca753';
+
+send(url)
+    .then(data => {
+        console.log(data);
+    })
+    .catch(error => {
+        console.log(error);
+    });
---
+++ day-22/homework-2.js
@@ -0,0 +1,31 @@
+class Countries {
+    constructor(url) {
+        if (typeof url !== 'string') throw new TypeError("`url` isn't string")
+        this.url = url
+    }
+    send(city) {
+        if (typeof city !== 'string') throw new TypeError("parameter isn't a string")
+        const Url = `https://api.openweathermap.org/data/2.5/weather?q=${city}&appid=299fb2133133f9d8fc214f5ae28ca753`
+        return new Promise((resolve, reject) => {
+            get(Url, (error, meta, body) => {
+                if (meta.status == 200) {
+                    const data = JSON.parse(body)
+                    resolve(data.wind)
+                }
+                reject(`We have error, status code: ${meta.status}`)
+            })
+        })
+    }
+}
+const get = require('fetch').fetchUrl;
+const url = 'https://api.openweathermap.org/data/2.5/weather?q=&appid=299fb2133133f9d8fc214f5ae28ca753';
+const countries = new Countries(url);
+
+(async() => {
+    try {
+        const data = await countries.send('Tbilisi');
+        console.log(data); // array of countries
+    } catch (error) {
+        console.log(error);
+    }
+})();
